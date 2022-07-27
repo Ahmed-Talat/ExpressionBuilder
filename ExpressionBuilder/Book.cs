640d425d607a96bc2a0d1fbdc412f1deaf9ef248
Ahmed Talat <talat.salem@gmail.com>
2022-07-27 08:50:37 +0200

    Add project files.

---
+++ ExpressionBuilder/Book.cs
@@ -0,0 +1,9 @@
+﻿internal class Book
+{
+    public Header Header { get; set; }
+}
+
+internal class Header
+{
+    public int Page { get; set; }
+}
---
+++ ExpressionBuilder/Program.cs
@@ -0,0 +1,9 @@
+﻿var query = (new List<Book>()).AsQueryable();
+
+var x = "Header.Page" ; 
+
+var predicate = ExpressionBuilder.BuildPredicate<Book>(12, OperatorComparer.LessThan, x.Split('.'));
+
+query = query.Where(predicate);
+
+Console.WriteLine("Expression Tree: {0}", predicate);
